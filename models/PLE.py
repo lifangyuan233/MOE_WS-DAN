2dcda8c957e88939266e5ef4fe58993d54d477c6
lifangyuan.233 <lifangyuan.233@bytedance.com>
2025-06-12 14:58:42 +0800

    20 不要共享专家 只有a b，各算各的没有相交

--- models/PLE.py
+++ models/PLE.py
@@ -50,22 +50,18 @@ class Expert_net(nn.Module):
 class CGCBlock(nn.Module):
     """层级化专家模块 (包含任务专属和共享专家)"""
     # 64/128   128  2  1  3
-    def __init__(self, in_channels, expert_channels, num_task_experts=1, num_shared_experts=1, GateNum=3):
+    def __init__(self, in_channels, expert_channels, num_task_experts=1, GateNum=2):
         super(CGCBlock, self).__init__()
 
         self.GateNum = GateNum #输出几个Gate的结果，2表示最后一层只输出两个任务的Gate，3表示还要输出中间共享层的Gate
         
         '''两个任务模块，一个共享模块'''
         self.n_task = 2
-        self.n_share = 1
+        self.n_share = 0
 
         self.taskA_experts = nn.ModuleList([
             Expert_net(in_channels, expert_channels) 
             for _ in range(num_task_experts)])
-        
-        self.shared_experts = nn.ModuleList([
-            Expert_net(in_channels, expert_channels) 
-            for _ in range(num_shared_experts)])
         
         self.taskB_experts = nn.ModuleList([
             Expert_net(in_channels, expert_channels) 
@@ -77,62 +73,44 @@ class CGCBlock(nn.Module):
             nn.Flatten(),
             nn.Linear(in_channels, 128),
             nn.ReLU(),
-            nn.Linear(128, num_task_experts + num_shared_experts),
+            nn.Linear(128, num_task_experts),
             nn.Softmax(dim=1))
-
-        if GateNum == 3:
-            self.gateS = nn.Sequential(
-                nn.AdaptiveAvgPool2d(1),
-                nn.Flatten(),
-                nn.Linear(in_channels, 128),
-                nn.ReLU(),
-                nn.Linear(128, 2*num_task_experts + num_shared_experts),
-                nn.Softmax(dim=1))
         
         self.gateB = nn.Sequential(
             nn.AdaptiveAvgPool2d(1),
             nn.Flatten(),
             nn.Linear(in_channels, 128),
             nn.ReLU(),
-            nn.Linear(128, num_task_experts + num_shared_experts),
+            nn.Linear(128, num_task_experts),
             nn.Softmax(dim=1))
         
 
-    def forward(self, x_A, x_S, x_B):
+    def forward(self, x_A, x_B):
         bs, _, h, w = x_A.shape
         
         # 生成专家特征
         experts_A_out = [e(x_A) for e in self.taskA_experts]  # 各专家输出形状: (bs, ec, h, w)
-        experts_S_out = [e(x_S) for e in self.shared_experts]
         experts_B_out = [e(x_B) for e in self.taskB_experts]
         
         # 门控权重计算
         gate_weights_A = self.gateA(x_A).unsqueeze(-1).unsqueeze(-1).unsqueeze(-1)  # (bs, num_experts, 1, 1, 1)
-        if self.GateNum == 3:
-            gate_weights_S = self.gateS(x_S).unsqueeze(-1).unsqueeze(-1).unsqueeze(-1)  # (bs, num_experts, 1, 1, 1)
         gate_weights_B = self.gateB(x_B).unsqueeze(-1).unsqueeze(-1).unsqueeze(-1)  # (bs, num_experts, 1, 1, 1)
         
         # 组合专家输出
-        combined_A = torch.stack(experts_A_out + experts_S_out, dim=1)  # (bs, num_total, ec, h, w)
+        combined_A = torch.stack(experts_A_out, dim=1)  # (bs, num_total, ec, h, w)
         Gate_A_out = (combined_A * gate_weights_A).sum(dim=1)
 
-        if self.GateNum == 3:
-            combined_S = torch.stack(experts_A_out + experts_S_out + experts_B_out, dim=1)  # (bs, num_total, ec, h, w)
-            Gate_S_out = (combined_S * gate_weights_S).sum(dim=1)
         
-        combined_B = torch.stack(experts_B_out + experts_S_out, dim=1)
+        combined_B = torch.stack(experts_B_out, dim=1)
         Gate_B_out = (combined_B * gate_weights_B).sum(dim=1)
-        
-        if self.GateNum == 3:
-            return Gate_A_out, Gate_S_out, Gate_B_out
-        else:
-            return Gate_A_out, Gate_B_out
+
+        return Gate_A_out, Gate_B_out
 
 
 
 class DeepExpertNetwork(nn.Module):
     """深度多专家网络"""
-    def __init__(self, in_channels=3, base_channels=64, expert_channels=128, num_blocks=2, num_task_experts=2, num_shared_experts=1, mode='train'):
+    def __init__(self, in_channels=3, base_channels=64, expert_channels=128, num_blocks=2, num_task_experts=2, mode='train'):
         super(DeepExpertNetwork, self).__init__()
         self.mode = mode
         # 基础特征提取
@@ -149,7 +127,7 @@ class DeepExpertNetwork(nn.Module):
             # 在特定层进行下采样
             stride = 2 if i in [1, num_blocks//2 + 1] else 1
             
-            block = CGCBlock(in_channels=current_channels, expert_channels=expert_channels, num_task_experts=num_task_experts, num_shared_experts=num_shared_experts, GateNum=3 if i < num_blocks-1 else 2) # 最后一层只用任务门控
+            block = CGCBlock(in_channels=current_channels, expert_channels=expert_channels, num_task_experts=num_task_experts, GateNum=2) # 最后一层只用任务门控
             self.blocks.append(block)
             current_channels = expert_channels
         
@@ -163,14 +141,10 @@ class DeepExpertNetwork(nn.Module):
         x0 = self.stem(x)  # 初始下采样
         
         a = x0
-        s = x0
         b = x0
 
         for index, block in enumerate(self.blocks):
-            if index != len(self.blocks) - 1:
-                a, s, b = block(a, s, b)
-            else:
-                a, b = block(a, s, b)
+            a, b = block(a, b)
 
         # print(a.shape, b.shape)   # a: [B, 128, 224, 224])   # b: [B, 128, 224, 224])
         # 任务预测
